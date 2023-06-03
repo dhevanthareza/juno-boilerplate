f65a94b3d3bcb2c0827d209c207b4a9c48591755
dhevanthareza <dhevanmuhamad@gmail.com>
2023-06-03 13:48:28 +0700

    remove geneerated file

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,6 @@ Route::middleware(['auth'])->group(function () {
     require app_path('Modules/Role/routes.php');
     require app_path('Modules/Permission/routes.php');
     require app_path('Modules/Module/routes.php');
-    require app_path('Modules/TesMenu/routes.php');
-    require app_path('Modules/Presensi/routes.php');
-    require app_path('Modules/Jabatan/routes.php');
-    require app_path('Modules/Shift/routes.php');
-    require app_path('Modules/Laptop/routes.php');
     
     // ROUTE_MARKER
     // Add routes in the line below (DONT REMOVE THIS SECTION !!!!!!, because this line is LINE_MARKER used by Module Generator)
