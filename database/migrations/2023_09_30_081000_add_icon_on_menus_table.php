eeea469f9be3852318d6c82dd74dc342a1e89fe3
dhevanthareza <dhevanmuhamad@gmail.com>
2023-10-03 14:43:43 +0700

    beneri migration

--- database/migrations/2023_09_30_081000_add_icon_on_menus_table.php
+++ database/migrations/2023_09_30_081000_add_icon_on_menus_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('menus', function (Blueprint $table) {
-            $table->string("icon");
+            $table->string("icon")->nullable();
         });
     }
 
